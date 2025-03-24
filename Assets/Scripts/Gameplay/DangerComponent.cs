0bfa9cb698dd6a6dd16bb77eb6c21f896fda445c
David Xu <davidx@usc.edu>
2025-03-24 14:34:59 -0700

    edits to moving danger prototype

--- Assets/Scripts/Gameplay/DangerComponent.cs
+++ Assets/Scripts/Gameplay/DangerComponent.cs
@@ -1,16 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class DangerComponent : MonoBehaviour
 {
+    [Header("Core Settings")]
     public float m_damage = 2.5f;
     public float m_timeOffset = 10.0f;
-    public bool m_canMove = false;
-   
     public bool m_oneTimeDamage = false;
+    
+    [Header("Movement Related")]
+    public bool m_canMove = false;
+    public float m_moveTime = 2.0f;
+    [SerializeField] private Ease m_moveStype;
+    [SerializeField] private GameObject m_end;
+    [SerializeField] private LineRenderer m_lineRenderer;
+    
     private bool m_canDamage = true;
+    private Sequence m_moveSequence;
     
     private void OnCollisionStay2D(Collision2D other)
     {
@@ -34,10 +43,45 @@ public class DangerComponent : MonoBehaviour
 
     private void Start()
     {
+        if (m_canMove && m_end != null && m_lineRenderer != null)
+        {
+            m_lineRenderer.SetPosition(1, m_end.transform.localPosition);
+            m_moveSequence = DOTween.Sequence();
+            TweenMove();
+        }
+        else if (m_canMove)
+        {
+            Debug.LogError("The danger can move, but is not setup correctly");
+        }
     }
 
-    private void Update()
+    private void OnDrawGizmos()
     {
+        if (m_canMove && m_end != null)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawLine(transform.position, m_end.transform.position);
+            Gizmos.DrawWireSphere(m_end.transform.position, 0.5f);
+        }
+    }
+
+    private void TweenMove()
+    {
+        m_moveSequence.Append(m_lineRenderer
+            .DOColor(new Color2(m_lineRenderer.startColor, m_lineRenderer.endColor),
+                new Color2(Color.white, Color.white), 0.25f)
+            .SetLoops(3, LoopType.Restart)
+            .OnComplete(() =>
+            {
+                m_lineRenderer.enabled = false;
+            }));
+        m_moveSequence.Append(transform
+            .DOMove(m_end.transform.position, m_moveTime)
+            .SetEase(m_moveStype)
+            .OnComplete(() =>
+            {
+                Destroy(gameObject);
+            }));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
