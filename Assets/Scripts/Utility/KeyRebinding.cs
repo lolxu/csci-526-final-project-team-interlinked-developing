1e281d1789cc9cd531943f9ce08c23c7fb0f9b75
David Xu <davidx@usc.edu>
2025-04-21 15:14:39 -0700

    Added duplicate binding check

--- Assets/Scripts/Utility/KeyRebinding.cs
+++ Assets/Scripts/Utility/KeyRebinding.cs
@@ -11,8 +11,10 @@ public class KeyRebinding : MonoBehaviour
     [SerializeField] private TMP_Text m_bindingDisplayText;
     [SerializeField] private GameObject m_startRebindObject;
     [SerializeField] private GameObject m_waitingForInputObject;
+    [SerializeField] private GameObject m_bindingWarning;
 
     private InputActionRebindingExtensions.RebindingOperation m_rebindOp;
+    private Coroutine m_warningCoroutine;
 
     private void Start()
     {
@@ -34,10 +36,26 @@ public class KeyRebinding : MonoBehaviour
             .OnComplete(operation =>
             {
                 int bindIdx = m_action.action.GetBindingIndexForControl(m_action.action.controls[0]);
+
+                if (CheckDuplicateBindings(m_action.action, bindIdx))
+                {
+                    if (m_warningCoroutine != null)
+                    {
+                        StopCoroutine(m_warningCoroutine);
+                    }
+                    m_warningCoroutine = StartCoroutine(ShowErrorMessage());
+                    
+                    m_action.action.RemoveBindingOverride(bindIdx);
+                    m_bindingDisplayText.text =
+                        InputControlPath.ToHumanReadableString(m_action.action.bindings[bindIdx].effectivePath);
+                    m_rebindOp.Dispose();
+                    m_startRebindObject.SetActive(true);
+                    m_waitingForInputObject.SetActive(false);
+                    return;
+                }
                 
                 m_bindingDisplayText.text =
                     InputControlPath.ToHumanReadableString(m_action.action.bindings[bindIdx].effectivePath);
-                
                 m_rebindOp.Dispose();
                 
                 m_action.action.Enable();
@@ -48,5 +66,43 @@ public class KeyRebinding : MonoBehaviour
             })
             .Start();
     }
-    
+
+    private bool CheckDuplicateBindings(InputAction action, int bindingIndex, bool allCompositeParts = false)
+    {
+        InputBinding newBinding = action.bindings[bindingIndex];
+        foreach (var binding in action.actionMap.bindings)
+        {
+            if (binding.action == newBinding.action)
+            {
+                continue;
+            }
+
+            if (binding.effectivePath == newBinding.effectivePath)
+            {
+                Debug.LogError("Duplicate Bindings!");
+                return true;
+            }
+        }
+
+        if (allCompositeParts)
+        {
+            for (int i = 1; i < bindingIndex; i++)
+            {
+                if (action.bindings[i].effectivePath == newBinding.overridePath)
+                {
+                    Debug.LogError("Duplicate Bindings!");
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    IEnumerator ShowErrorMessage()
+    {
+        m_bindingWarning.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        m_bindingWarning.SetActive(false);
+    }
 }
