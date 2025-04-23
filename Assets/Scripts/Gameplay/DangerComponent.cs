27d3e69f82316f84e51423493b86d9073baef978
David Xu <davidx@usc.edu>
2025-04-23 15:32:09 -0700

    Added piston style moving trap

--- Assets/Scripts/Gameplay/DangerComponent.cs
+++ Assets/Scripts/Gameplay/DangerComponent.cs
@@ -15,12 +15,31 @@ public class DangerComponent : MonoBehaviour
     public bool m_canMove = false;
     public float m_moveTime = 2.0f;
     [SerializeField] private Ease m_moveStyle;
-    [SerializeField] private GameObject m_end;
+    public List<GameObject> m_pathObjects = new List<GameObject>();
+    [SerializeField] private GameObject m_telegraphEnd;
     [SerializeField] private LineRenderer m_lineRenderer;
-    
+
+    private Vector3 m_orgPosition;
     private bool m_canDamage = true;
     private Sequence m_moveSequence;
-    
+
+    private void Start()
+    {
+        m_orgPosition = transform.position;
+        if (m_canMove && m_telegraphEnd != null && m_lineRenderer != null)
+        {
+            Vector3 telegraphEndPos = m_telegraphEnd.transform.localPosition;
+            // telegraphEndPos.y += transform.localScale.y / 4.0f;
+            m_lineRenderer.SetPosition(1, telegraphEndPos);
+            m_moveSequence = DOTween.Sequence();
+            TweenMove();
+        }
+        else if (m_canMove)
+        {
+            Debug.LogError("The danger can move, but is not setup correctly");
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (m_canDamage)
@@ -43,48 +62,59 @@ public class DangerComponent : MonoBehaviour
         }
     }
 
-
-    private void Start()
-    {
-        if (m_canMove && m_end != null && m_lineRenderer != null)
-        {
-            m_lineRenderer.SetPosition(1, m_end.transform.localPosition);
-            m_moveSequence = DOTween.Sequence();
-            TweenMove();
-        }
-        else if (m_canMove)
-        {
-            Debug.LogError("The danger can move, but is not setup correctly");
-        }
-    }
-
     private void OnDrawGizmos()
     {
-        if (m_canMove && m_end != null)
+        if (m_canMove && m_telegraphEnd != null)
         {
             Gizmos.color = Color.magenta;
-            Gizmos.DrawLine(transform.position, m_end.transform.position);
-            Gizmos.DrawWireSphere(m_end.transform.position, 0.5f);
+            Gizmos.DrawLine(transform.position, m_telegraphEnd.transform.position);
+            Gizmos.DrawWireSphere(m_telegraphEnd.transform.position, 0.5f);
         }
     }
 
     private void TweenMove()
     {
+        Color orgStartColor = m_lineRenderer.startColor;
+        Color orgEndColor = m_lineRenderer.endColor;
         m_moveSequence.Append(m_lineRenderer
             .DOColor(new Color2(m_lineRenderer.startColor, m_lineRenderer.endColor),
                 new Color2(Color.white, Color.white), 0.25f)
             .SetLoops(3, LoopType.Restart)
             .OnComplete(() =>
             {
+                m_lineRenderer.startColor = orgStartColor;
+                m_lineRenderer.endColor = orgEndColor;
                 m_lineRenderer.enabled = false;
             }));
-        m_moveSequence.Append(transform
-            .DOMove(m_end.transform.position, m_moveTime)
-            .SetEase(m_moveStyle)
-            .OnComplete(() =>
-            {
-                Destroy(gameObject);
-            }));
+
+        float pathMoveTime = m_moveTime / m_pathObjects.Count;
+        foreach (var pathObject in m_pathObjects)
+        {
+            Vector3 pathPos = pathObject.transform.position;
+            Bounds bounds = GetComponent<Renderer>().bounds;
+            float heightOffset = bounds.extents.y;
+            Vector3 adjustedPos = new Vector3(
+                pathPos.x,
+                pathPos.y - heightOffset, // Subtract the height offset to align top with pathPos
+                pathPos.z
+            );
+            m_moveSequence.Append(transform
+                .DOMove(adjustedPos, pathMoveTime)
+                .SetEase(m_moveStyle));
+        }
+
+        m_moveSequence.OnComplete(() =>
+        {
+            Destroy(gameObject);
+        });
+        
+        // m_moveSequence.Append(transform
+        //     .DOMove(m_end.transform.position, m_moveTime)
+        //     .SetEase(m_moveStyle)
+        //     .OnComplete(() =>
+        //     {
+        //         Destroy(gameObject);
+        //     }));
     }
 
     // Use trigger for moving danger
--- Assets/Scripts/Gameplay/ObstacleSpawner.cs
+++ Assets/Scripts/Gameplay/ObstacleSpawner.cs
@@ -6,9 +6,11 @@ using Random = UnityEngine.Random;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    [Header("Trap Spawner Settings")]
     public List<GameObject> m_traps;
     public float m_minTimeout = 5.0f;
     public float m_maxTimeout = 20.0f;
+    public bool m_isRandomSpawn = true;
 
     private float m_timeout = 0.0f;
     private float m_timer = 0.0f;
@@ -26,18 +28,30 @@ public class ObstacleSpawner : MonoBehaviour
         {
             m_timer = 0.0f;
             m_timeout = Random.Range(m_minTimeout, m_maxTimeout);
-            
-            SpawnObstacleRandomly();
+
+            if (m_isRandomSpawn)
+            {
+                SpawnObstacleRandomly();
+            }
+            else
+            {
+                SpawnObstacle();
+            }
         }
 
         m_timer += Time.deltaTime;
+    }
+
+    private void SpawnObstacle()
+    {
+        Vector3 spawnPos = transform.position;
+        Instantiate(m_traps[Random.Range(0, m_traps.Count)], spawnPos, transform.rotation);
     }
 
     private void SpawnObstacleRandomly()
     {
         float randX = Random.Range(-m_collider.size.x / 2.0f + transform.position.x, m_collider.size.x / 2.0f + transform.position.x);
         Vector3 spawnPos = new Vector3(randX, transform.position.y, 0.0f);
-
         Instantiate(m_traps[Random.Range(0, m_traps.Count)], spawnPos, transform.rotation);
     }
 
